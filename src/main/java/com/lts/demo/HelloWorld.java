bf2390689663454011c1f32cb7c2fdae77e29f65
Neha <neha.jain216@gmail.com>
2017-03-04 10:18:55 +0530

    Init.

---
+++ src/main/java/com/lts/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lts.demo;
+
+/**
+ * Created by Neha on 3/4/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
---
+++ src/test/java/com/lts/demo/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.lts.demo;
+
+import org.junit.Test;
+
+/**
+ * Created by Neha on 3/4/2017.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void test()
+    {
+
+    }
+}
